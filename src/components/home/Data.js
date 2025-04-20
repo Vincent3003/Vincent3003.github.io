50b92165ef8537d490668d4dbcfd030d89d575e9
ChiDang0803 <changdang1999@gmail.com>
2025-04-19 19:45:08 -0500

    color change

--- src/components/home/Data.js
+++ src/components/home/Data.js
@@ -60,8 +60,8 @@ const Data = () => {
 
           <h3 className="home-subtitle">Data Analyst</h3>
           <p className="home-description">
-          I thrive on transforming data into meaningful insights and continuously strive to sharpen my skills 
-          in this fast-evolving, data-driven world.
+          I am passionate in transforming data into meaningful insights and continuously striving to sharpen my skills 
+          in this ever-changing, data-driven world.
           </p>
 
           <div className="button-container">
